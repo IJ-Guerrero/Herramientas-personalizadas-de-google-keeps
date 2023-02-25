a3c06b56e84b221222971b317d2a36c347db8a59
IJ-Guerrero <109167544+IJ-Guerrero@users.noreply.github.com>
2023-02-25 17:59:36 -0300

    20230225-3

--- notesGetter.js
+++ notesGetter.js
@@ -1,3 +1,4 @@
+//Esta función permite obtener el título y el contnido de todas las notas de google keep de la cuenta actualmente habierta
 function notesGetter(){
   let $titulos = [...document.querySelectorAll("div.IZ65Hb-s2gQvd div.IZ65Hb-r4nke-haAclf div.notranslate.IZ65Hb-YPqjbf.fmcmS-x3Eknd.r4nke-YPqjbf")].map(function(el){return el.innerText})
   let $texto = [...document.querySelectorAll("div.IZ65Hb-s2gQvd div.IZ65Hb-qJTHM-haAclf div.notranslate.IZ65Hb-YPqjbf.fmcmS-x3Eknd.h1U9Be-YPqjbf")].map(function(el){return el.innerText})
